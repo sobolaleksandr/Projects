e7f77eb00beae3fd38f557c35d3b359c63291ef8
sobolaleksandr <78675732+sobolaleksandr@users.noreply.github.com>
2021-02-16 11:20:54 +0700

    Update Person.cs

--- С#/HallOfFame/Models/Person.cs
+++ С#/HallOfFame/Models/Person.cs
@@ -10,8 +10,8 @@ namespace HallOfFame.Models
         public int id { get; set; }
         public string name { get; set; }
         public string displayName { get; set; }
-        public Skill skills { get; set; }
-    //public List<Skill> skills { get; set; }
-    //public int SkillId { get; set; }
+        public Skill[] skills { get; set; }
+
+        //public int SkillId { get; set; }
 }
 }
