4fb4085829ac74918fc88b707fe829cbe909b1ba
Sobol Alexander <sobolaleksandr96@gmail.com>
2021-02-22 11:19:02 +0700

    Создать проект Книжный магазин

---
+++ С#/Store/Store.Memory/BookRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store
+{
+
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        {
+        new Book(1, "Art Of Programming"),
+        new Book(1, "Refactoring"),
+        new Book(1, "C Programming Language")
+        };
+
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart))
+                .ToArray();
+        }
+
+    }
+}
