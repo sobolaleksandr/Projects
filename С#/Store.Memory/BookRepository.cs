9488691909e2a000a46e292099a7f50fa1132e85
Sobol Alexander <sobolaleksandr96@gmail.com>
2021-02-22 17:17:39 +0700

    Добавить страницу описание книги

--- С#/Store.Memory/BookRepository.cs
+++ С#/Store.Memory/BookRepository.cs
@@ -10,9 +10,17 @@ namespace Store
     {
         private readonly Book[] books = new[]
         {
-            new Book(1, "ISBN 12312-31231", "D. Knuth", "Art Of Programming"),
-            new Book(1, "ISBN 12312-31232", "M. Fowler", "Refactoring"),
-            new Book(1, "ISBN 12312-31233", "B. Kernighan, D. Ritchie", "C Programming Language")
+            new Book(1, "ISBN 0201038013", "D. Knuth", "Art Of Programming, Vol. 1",
+                     "This volume begins with basic programming concepts and techniques, then focuses more particularly on information structures-the representation of information inside a computer, the structural relationships between data elements and how to deal with them efficiently.",
+                     7.19m),
+
+            new Book(2, "ISBN 0201485672", "M. Fowler", "Refactoring",
+                     "As the application of object technology--particularly the Java programming language--has become commonplace, a new problem has emerged to confront the software development community.",
+                     12.45m),
+
+            new Book(3, "ISBN 0131101633", "B. W. Kernighan, D. M. Ritchie", "C Programming Language",
+                     "Known as the bible of C, this classic bestseller introduces the C programming language and illustrates algorithms, data structures, and programming techniques.",
+                     14.98m),
         };
 
         public Book[] GetAllByIsbn(string isbn)
@@ -25,8 +33,12 @@ namespace Store
         {
             return books.Where(book => book.Author.Contains(query)
                                    || book.Title.Contains(query))
-                .ToArray();
+                        .ToArray();
         }
 
+        public Book GetById(int id)
+        {
+            return books.Single(book => book.Id == id);
+        }
     }
 }
