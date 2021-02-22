7ec730d8c8c221ae1d372e53d829d3b0d1cce2e9
Sobol Alexander <sobolaleksandr96@gmail.com>
2021-02-22 15:48:01 +0700

    Реализовать поиск по ISBN и автору

---
+++ С#/Store.Memory/BookRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store
+{
+
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "ISBN 12312-31231","D. Knuth", "Art Of Programming"),
+            new Book(1, "ISBN 12312-31232","M. Fowler", "Refactoring"),
+            new Book(1, "ISBN 12312-31233","B. Kernighan, D. Ritchie", "C Programming Language")
+        };
+
+        public Book[] GetAllByIsbn(string isbn)
+        {
+            return books.Where(book => book.Isbn == isbn)
+                        .ToArray();
+        }
+
+        public Book[] GetAllByTitleOrAuthor(string query)
+        {
+            return books.Where(book => book.Author.Contains(query)
+                                   || book.Title.Contains(query))
+                .ToArray();
+        }
+
+    }
+}
