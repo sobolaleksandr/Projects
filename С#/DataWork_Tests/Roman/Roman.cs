0159381ecd16fad31346deb1fef7b5645830273c
sobolaleksandr <78675732+sobolaleksandr@users.noreply.github.com>
2021-03-12 16:46:40 +0700

    Добавить файлы

--- С#/DataWork_Tests/Roman/Roman.cs
+++ С#/DataWork_Tests/Roman/Roman.cs
@@ -4,28 +4,17 @@ namespace Roman
 {
     public static class Roman
     {
-        private static int decodeSingle(char letter)
+        private static int DecodeSingle(char letter) => letter switch
         {
-            switch (letter)
-            {
-                case 'M':
-                    return 1000;
-                case 'D':
-                    return 500;
-                case 'C':
-                    return 100;
-                case 'L':
-                    return 50;
-                case 'X':
-                    return 10;
-                case 'V':
-                    return 5;
-                case 'I':
-                    return 1;
-                default:
-                    throw new ArgumentException("String must be Roman!");
-            }
-        }
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+            _ => throw new ArgumentException("String must be Roman!"),
+        };
 
         public static int RomanToInt(string roman)
         {
@@ -39,16 +28,13 @@ namespace Roman
             for (int i = 0; i < stringLength - 1; i++)
             {
                 //loop over all but the last character
-                if (decodeSingle(uRoman[i]) < decodeSingle(uRoman[i + 1]))
-                {
-                    result -= decodeSingle(uRoman[i]);
-                }
+                if (DecodeSingle(uRoman[i]) < DecodeSingle(uRoman[i + 1]))
+                    result -= DecodeSingle(uRoman[i]);
                 else
-                {
-                    result += decodeSingle(uRoman[i]);
-                }
+                    result += DecodeSingle(uRoman[i]);
             }
-            result += decodeSingle(uRoman[uRoman.Length- 1]);
+
+            result += DecodeSingle(uRoman[stringLength - 1]);
             return result;
         }
     }
